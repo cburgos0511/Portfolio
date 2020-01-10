fa5d152db340b5acb34707302f672ab2a47d1063
Cruz Burgos <48486237+cburgos0511@users.noreply.github.com>
2020-01-10 12:44:09 -0600

    added validation to contact form

--- scripts/form.js
+++ scripts/form.js
@@ -13,8 +13,29 @@ $(document).ready(() => {
 		if (email.length > 5 && email.includes("@" && email.includes("."))) {
 			statusEl.append("<div>Email good</div>");
 		} else {
-			event.preventDefault();
 			statusEl.append("<div>Email not good</div>");
+			event.preventDefault();
+		}
+
+		if (name > 2) {
+			statusEl.append("<div>Name good</div>");
+		} else {
+			statusEl.append("<div>Name not good</div>");
+			event.preventDefault();
+		}
+
+		if (subject > 2) {
+			statusEl.append("<div>Subject good</div>");
+		} else {
+			statusEl.append("<div>Subject not good</div>");
+			event.preventDefault();
+		}
+
+		if (msg > 15) {
+			statusEl.append("<div>Msg good</div>");
+		} else {
+			statusEl.append("<div>Msg not good</div>");
+			event.preventDefault();
 		}
 	});
 });
